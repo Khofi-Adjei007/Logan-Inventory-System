ad45e87a7ec1b2b8995c9d8253facd2572b86d29
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-01-16 23:21:07 +0000

    Configured Tailwind but incomplete

--- logan_inventory_system/settings.py
+++ logan_inventory_system/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+    'authentications'
 ]
+
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
