b2abda8cf0708bd33d2f999256df90fae847ffb8
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-04-12 09:15:32 +0000

    Access page: (Started building access page from login page

--- logan_inventory_system/settings.py
+++ logan_inventory_system/settings.py
@@ -90,13 +90,11 @@ WSGI_APPLICATION = 'logan_inventory_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'loganinventories',
-        'USER': 'root',
-        'PASSWORD': '@Tacitus0202',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
